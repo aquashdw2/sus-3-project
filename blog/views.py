2462d4d959048cbf6dfd86c08d42a2508d43a7cd
aquashdw <aquashdw@gmail.com>
2022-07-03 11:01:54 +0900

    update blog

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,15 @@ from blog.models import Article
 
 
 def blog_home(request):
-    return render(request, "blog-html/index.html")
+    post_all = Article.objects.all().order_by("-pk")
+    context = {
+        "post_list": post_all
+    }
+    return render(
+        request,
+        "blog-html/index.html",
+        context
+    )
 
 
 def blog_post_create(request):
@@ -19,43 +27,50 @@ def blog_post_create(request):
         return render(request, "blog-html/post-create.html")
 
 
-def blog_post_read(request):
-    return render(request, "blog-html/post-read.html")
+def blog_post_read(request, target_id):
+    target_post = Article.objects.get(id=target_id)
+    context = {
+        "post": target_post,
+    }
+    return render(
+        request, 
+        "blog-html/post-read.html",
+        context
+    )
 
 
-def blog_post_update(request):
-    return render(request, "blog-html/post-update.html")
+def blog_post_update(request, target_id):
+    target_post = Article.objects.get(id=target_id)
+    if request.method == "POST":
+        target_post.title = request.POST.get("title")
+        target_post.content = request.POST.get("content")
+        target_post.save()
+
+        return redirect(f"/blog-url/{target_id}/read/")
+
+    elif request.method == "GET":
+        context = {
+            "post": target_post,
+        }
+
+        return render(
+            request,
+            "blog-html/post-update.html", 
+            context
+        )
 
 
-def blog_post_delete(request):
-    return render(request, "blog-html/post-delete.html")
-
-
-def temp():
-    # Create
-    new_post = Article()
-    new_post.title = "new post title"
-    new_post.content = "Lorem Ipsum"
-    new_post.save()
-
-
-    # Read
-    post_list = Article.objects.all()
-    for post in post_list:
-        print(post.title)
-    first_post = Article.objects.get(id=1)
-    print(f"title: {first_post.title}")
-    print(f"content: {first_post.content}")
-    print(f"created: {first_post.created_at}")
-
-
-    # Update
-    target_post = Article.objects.get(id=3)
-    target_post.title = "Blog Title 3"
-    target_post.content = "new updated content"
-    target_post.save()
-
-
-    # Delete
-    target_post = Article.objects.order_by("-pk")[0]
-    target_post.delete()
+def blog_post_delete(request, target_id):
+    target_post = Article.objects.get(id=target_id)
+    if request.method == "POST":
+        target_post.delete()
+        return redirect("/blog-url/home/")
+    elif request.method == "GET":
+        context = {
+            "post": target_post
+        }
+        return render(
+            request, 
+            "blog-html/post-delete.html",
+            context
+        )
