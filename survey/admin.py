cb6a19dd13d7bf9fbd11eb8eda652f5aa1667b86
aquashdw <aquashdw@gmail.com>
2022-07-03 10:36:15 +0900

    simple create

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from survey import models
+
+
+admin.site.register(models.SurveySimple)
+admin.site.register(models.SurveyChoices)
--- survey/views.py
+++ survey/views.py
@@ -5,22 +5,27 @@ from survey.models import SurveySimple, SurveyChoices
 
 def create_survey_simple(request):
     if request.method == "POST":
-        
-        print(request.POST.get("age"))
-        print(request.POST.get("gender"))
-        print(request.POST.get("address"))
-        print(request.POST.get("suggestion"))
-        return redirect("/polls/submit-simple/")
+        new_survey = SurveySimple()
+        new_survey.age = request.POST.get("age")
+        new_survey.gender = request.POST.get("gender")
+        new_survey.address = request.POST.get("address")
+        new_survey.suggestions = request.POST.get("suggestions")
+        new_survey.save()
+
+        return redirect("/survey/submit-simple/")
     elif request.method == "GET":
         return render(request, "polls/simple.html")
 
 
 def create_survey_choice(request):
     if request.method == "POST":
-        print(request.POST.get("age"))
-        print(request.POST.get("gender"))
-        print(request.POST.get("address"))
-        print(request.POST.get("suggestion"))
-        return redirect("/polls/submit-choice/")
+        new_survey = SurveyChoices()
+        new_survey.age = request.POST.get("age")
+        new_survey.gender = request.POST.get("gender")
+        new_survey.address = request.POST.get("address")
+        new_survey.suggestions = request.POST.get("suggestions")
+        new_survey.save()
+
+        return redirect("/survey/submit-choice/")
     elif request.method == "GET":
         return render(request, "polls/choice.html")
